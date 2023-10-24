1525be59e3696128635cbad00016f3442c28ac67
Str0ke_ <charlelie.garrigues@gmail.com>
2023-10-24 13:54:04 +0200

    Add Ball selection

--- A4MobileJam/Assets/Scripts/GlobalManager.cs
+++ A4MobileJam/Assets/Scripts/GlobalManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -19,24 +20,30 @@ public class GlobalManager : MonoBehaviour
     [SerializeField] List<Material> _balls;
     [SerializeField] List<Sprite> _ballsSpr;
 
+
+    [SerializeField] GameObject _ballSelect;
+    int _currBallSelectIndex = 0;
+
     public struct SetP
     {
         public Image _img;
         public InputField _name;
         public Material _mat;
         public Sprite _spr;
+        public int _index;
         public string _strName
         {
             get => _name.text;
             set => _name.text = value;
         }
 
-        public SetP(Image image, InputField name, Material mat, Sprite spr)
+        public SetP(Image image, InputField name, Material mat, Sprite spr, int index)
         {
             _img = image;
             _name = name;
             _mat = mat;
             _spr = spr;
+            _index = index;
             _strName = _name.text;
             _name.contentType = InputField.ContentType.Name;
             _name.inputType = InputField.InputType.Standard;
@@ -96,12 +103,35 @@ public class GlobalManager : MonoBehaviour
             int rndBall = Random.Range(0, nbrs.Count);
             nbrs.RemoveAt(rndBall);
             GameObject p = Instantiate(_playerData, _layoutGroup.transform);
-            SetP set = new SetP(p.transform.GetChild(0).GetComponent<Image>(), p.transform.GetChild(1).GetComponent<InputField>(), _balls[rndBall], _ballsSpr[rndBall]);
+            SetP set = new SetP(p.transform.GetChild(0).GetComponent<Image>(), p.transform.GetChild(1).GetComponent<InputField>(), _balls[rndBall], _ballsSpr[rndBall], i);
             set._strName = "Player_" + (i+1).ToString();
             _sets.Add(set);
             set._img.sprite = _ballsSpr[rndBall];
             arrayPSet.Add(p);
+            p.GetComponent<PlayerSetIndex>().SetIndex(i);
         }
     }
-        
+       
+    public void ShowBallSelect(int playerIndex)
+    {
+        _ballSelect.SetActive(true);
+        _currBallSelectIndex = playerIndex;
+        _ballSelect.GetComponent<SelectBall>().Populate(_ballsSpr);
+    }
+
+    public void HideBallSelect(int selected)
+    {
+        _ballSelect.SetActive(false);
+        for (int i = _ballSelect.transform.GetChild(0).childCount - 1; i > 0; i--)
+        {
+            Destroy(_ballSelect.transform.GetChild(0).GetChild(i).gameObject);
+        }
+        Destroy(_ballSelect.transform.GetChild(0).GetChild(0).gameObject);
+
+        SetP p = _sets[_currBallSelectIndex];
+        p._mat = _balls[selected];
+        p._spr = _ballsSpr[selected];
+        _sets[_currBallSelectIndex] = p;
+        _layoutGroup.transform.GetChild(_currBallSelectIndex).GetChild(0).GetComponent<Image>().sprite = p._spr;
+    }
 }
